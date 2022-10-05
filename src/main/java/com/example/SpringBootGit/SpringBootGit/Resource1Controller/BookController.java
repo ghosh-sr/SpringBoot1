7ccdcdf03df5935c5f91e6c42c251a6362b9c3f7
ghosh-sr <srinjoyghosh999@gmail.com>
2022-10-05 12:44:22 +0530

    Book Controller Created

---
+++ src/main/java/com/example/SpringBootGit/SpringBootGit/Resource1Controller/BookController.java
@@ -0,0 +1,14 @@
+package com.example.SpringBootGit.SpringBootGit.Resource1Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BookController {
+
+	@RequestMapping("/home")
+	public String home() {
+		return "This is home page";
+	}
+	
+}
