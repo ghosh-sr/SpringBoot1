38dedc74e944df59381a794e433e3410673b2e48
ghosh-sr <srinjoyghosh999@gmail.com>
2022-10-05 12:14:10 +0530

    First commit

---
+++ src/test/java/com/example/SpringBootGit/SpringBootGit/SpringBootGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootGit.SpringBootGit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
